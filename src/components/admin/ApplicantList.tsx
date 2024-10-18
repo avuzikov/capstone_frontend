b29ed9010e2e0ba2bd0c48da5f06475e47cfe49a
matt54633 <matt54633@gmail.com>
2024-10-18 12:58:12 +0100

    Add Initial Component and Page Declarations

---
+++ src/components/admin/ApplicantList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ApplicantList = () => {
+  return <></>;
+};
+
+export default ApplicantList;
---
+++ src/components/admin/ManagerForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManagerForm = () => {
+  return <></>;
+};
+
+export default ManagerForm;
