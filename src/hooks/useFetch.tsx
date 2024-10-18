55407388dee88bab32b1756d42e4a75b37b692b0
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-10-18 22:25:10 +0200

    feat - change fetchDispatch to allow for entering data

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,23 +1,29 @@
 import { useState, useCallback } from "react";
 
-const useFetch = (fetchFn: () => Promise<any>, initialValue?: any) => {
+const useFetch = <T,>(
+  fetchFn: (data: T) => Promise<any>,
+  initialValue?: any
+) => {
   const [data, setData] = useState(initialValue);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState<null | { message: string }>(null);
 
-  const fetchDispatch = useCallback(async () => {
-    setIsPending(true);
-    setError(null);
+  const fetchDispatch = useCallback(
+    async (data: T) => {
+      setIsPending(true);
+      setError(null);
 
-    try {
-      const response = await fetchFn();
-      setData(response);
-    } catch (error: any) {
-      setError({ message: error.message || "Failed to fetch data" });
-    } finally {
-      setIsPending(false);
-    }
-  }, [fetchFn]);
+      try {
+        const response = await fetchFn(data);
+        setData(response);
+      } catch (error: any) {
+        setError({ message: error.message || "Failed to fetch data" });
+      } finally {
+        setIsPending(false);
+      }
+    },
+    [fetchFn]
+  );
 
   return { data, isPending, error, fetchDispatch };
 };
