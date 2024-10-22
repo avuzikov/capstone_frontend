ce2d59c3b9718f620495bfabebba658c8e722c4f
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-22 11:47:11 +0100

    Profile Work

--- src/pages/applicant/ProfilePage.tsx
+++ src/pages/applicant/ProfilePage.tsx
@@ -1,40 +1,64 @@
 import React from 'react';
 import UserForm from '../../components/admin/UserForm';
 import { useAuth } from '../../contexts/AuthContext';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { User } from '../../types/types';
 
 const ProfilePage = () => {
-    const { id, token } = useAuth();
-    const [showForm, setShowForm] = useState(true);
-    const [user, setUser] = useState<User | null>(null);
+  const { id, token } = useAuth();
+  const [showForm, setShowForm] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
-    const fetchUser = useCallback(async () => {
-        try {
-            const response = await fetch(`/users/${id}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+  const fetchUser = useCallback(async () => {
+    try {
+      const response = await fetch(`/users/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-            if (!response.ok) {
-                throw new Error('Failed to fetch users');
-            }
+      if (!response.ok) {
+        throw new Error('Failed to fetch users');
+      }
 
-            const data: User = await response.json();
-            setUser(data);
-        } catch (error) {
-            console.error('Failed to fetch user data:', error);
-        }
-    }, [id, token]);
+      const data: User = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.error('Failed to fetch user data:', error);
+    }
+  }, [id, token]);
 
-    return (
-        <div className="flex flex-col gap-3">
-            {showForm && <UserForm isEditing={true} userId={id || ''} />}
-        </div>
-    );
+  useEffect(() => {
+    if (token) {
+      fetchUser();
+    }
+  }, [token, fetchUser]);
+
+  return (
+    <>
+      <div className="flex flex-col m-large gap-3">
+        {/* dislay user details */}
+        {user && (
+          <div className="card-bordered">
+            <div>{user.fullName}</div>
+            <div>{user.email}</div>
+            <div>{user.address}</div>
+            <div>{user.phone}</div>
+            <div>{user.resume}</div>
+          </div>
+        )}
+        <button onClick={() => setShowForm(!showForm)} className='btn-secondary'>
+          {showForm ? 'Hide Form' : 'Edit Profile'}
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        {showForm && <UserForm isEditing={true} userId={id || ''} />}
+      </div>
+    </>
+  );
 };
 
 export default ProfilePage;
