96efa34347016e6059a6d8285e8aa3765ec21920
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-22 10:43:56 +0100

    Begin Profile Modification

--- src/pages/applicant/ProfilePage.tsx
+++ src/pages/applicant/ProfilePage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import UserForm from '../../components/admin/UserForm';
 import { useAuth } from '../../contexts/AuthContext';
+import { useState } from 'react';
 
 const ProfilePage = () => {
   const { id } = useAuth();
+  const [showForm, setShowForm] = useState(true);
 
-  return <UserForm isEditing={true} userId={id || ''} />;
+  return (
+    <div className="flex flex-col gap-3">
+      {} <UserForm isEditing={true} userId={id || ''} />
+    </div>
+  );
 };
 
 export default ProfilePage;
