be50a356f3668a33a1ae8ceec18a62528b8f7f5e
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-22 13:14:51 +0100

    Finalise Profile Changes

--- src/pages/applicant/ProfilePage.tsx
+++ src/pages/applicant/ProfilePage.tsx
@@ -30,34 +30,56 @@ const ProfilePage = () => {
     }
   }, [id, token]);
 
+  const handleShowForm = () => {
+    setShowForm(!showForm);
+  };
+
   useEffect(() => {
     if (token) {
       fetchUser();
     }
-  }, [token, fetchUser]);
+  }, [token, fetchUser, showForm, handleShowForm]);
 
   return (
-    <>
-      <div className="flex flex-col m-large gap-3">
-        {/* dislay user details */}
+    <div className="w-full flex items-center  flex-col justify-center">
+      <div className="flex flex-col w-full gap-3 m-medium lg:w-1/2 justify-center">
         {user && (
-          <div className="card-bordered">
-            <div>{user.fullName}</div>
-            <div>{user.email}</div>
-            <div>{user.address}</div>
-            <div>{user.phone}</div>
-            <div>{user.resume}</div>
+          <div>
+            <h1 className="text-large pb-2">Profile</h1>
+            <div className="card-bordered flex gap-4 flex-col">
+              <div className="m-small">
+                {Object.entries(user).map(([key, value]) => {
+                  if (key === 'id') {
+                    return null;
+                  }
+                  return (
+                    <div className="mb-3" key={key}>
+                      <p className="text-small pl-small">
+                        {key.charAt(0).toUpperCase() + key.slice(1)}
+                      </p>
+                      <div className="card-bordered">{value}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         )}
-        <button onClick={() => setShowForm(!showForm)} className='btn-secondary'>
+        <button onClick={() => setShowForm(!showForm)} className="btn-primary">
           {showForm ? 'Hide Form' : 'Edit Profile'}
         </button>
       </div>
 
-      <div className="flex flex-col gap-3">
-        {showForm && <UserForm isEditing={true} userId={id || ''} />}
+      <div className="flex flex-col w-full gap-3">
+        {showForm && (
+          <UserForm
+            isEditing={true}
+            userId={user?.id?.toString()}
+            handleShowForm={handleShowForm}
+          />
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
