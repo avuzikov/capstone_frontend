36e5237b240b17f7bb825e36eaa5d118aad2bbe6
avuzikov <avuzikov@gmail.com>
2024-10-24 12:40:21 +0200

    upd index.js

--- src/index.js
+++ src/index.js
@@ -40,3 +40,29 @@ if (process.env.NODE_ENV === 'development') {
     })
     .catch(console.error);
 }
+
+
+// Below code without Mock API
+
+/*
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
+
+*/
